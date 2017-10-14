d81ed4b5a4f771d5f2fa61518958b6f049a27129
Patryk L <patryk.lesiecki@windowslive.com>
2017-10-14 15:53:10 +0200

    Rewrited configuration system for UserData

--- src/main/java/pl/mplauncher/launcher/api/config/templates/UserDataTemplate.java
+++ src/main/java/pl/mplauncher/launcher/api/config/templates/UserDataTemplate.java
@@ -4,7 +4,7 @@ import org.diorite.cfg.annotations.CfgClass;
 import org.diorite.cfg.annotations.CfgComment;
 import org.diorite.cfg.annotations.defaults.CfgDelegateDefault;
 
-@CfgClass(name = "UserData")
+@CfgClass(name = "UserDataTemplate")
 @CfgDelegateDefault("{new}")
 @CfgComment("-------------------------------------------------------------------")
 @CfgComment("DON'T MODIFY THIS FILE ON YOUR OWN,")
@@ -14,7 +14,7 @@ import org.diorite.cfg.annotations.defaults.CfgDelegateDefault;
 @CfgComment("- save your custom settings")
 @CfgComment("- keep your favorite servers")
 @CfgComment("-------------------------------------------------------------------")
-public class UserData {
+public class UserDataTemplate {
 
     //TODO: Add more data about: favorite servers, resolution, sound settings, ram etc.
 
